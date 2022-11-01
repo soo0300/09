2793b0e51dcf63593168f4c36d4669c620d6d9b2
soo0300 <soo0300@naver.com>
2022-11-01 20:58:17 +0900

    실습3

--- main.c
+++ main.c
@@ -3,17 +3,25 @@
 #define SIZE 5
 
 int main(void){
-	int i, average;
-	int sum=0;
-	int grade[SIZE];
-	int score[SIZE];
+	int i;
+	int a[SIZE]={1,2,3,4,50};
+	int b[SIZE]={1,2,300,4,5};
+	
+	int flag_same=1;
+	if(a==b){
+		printf("배열은 같다\n");
+	}else{
+		printf("배열이 다르다\n");
+	} 
+	
+	for(i=0; i<SIZE; i++){
+		if(a[i]!=b[i]){
+			flag_same=0;
+		}
+		if(flag_same==0){
+			printf("%d index 배열 값끼리는 다르다\n",i);
+		}
+		flag_same=1;
+	}
 
-	for(i=0; i<SIZE; i++){
-		grade[i]=rand()%100;
-		score[i]=grade[i];
-	}
-	for(i=0; i<SIZE; i++){
-		printf("score[%d]=%d\n",i,score[i]);
-	}
-	return 0;
 }
