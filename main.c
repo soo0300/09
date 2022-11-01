c2a0a1a48d53dfb2e118388aa2b6ed9676d1852c
soo0300 <soo0300@naver.com>
2022-11-01 20:32:06 +0900

    실습_2(2)

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 #define SIZE 5
 
 int main(void){
-	int i;
+	int i, average;
+	int sum=0;
 	int grade[SIZE];
-	printf("5명의 점수를 입력하세요. \n");
-	
-	for(i=0; i<SIZE; i++){
+	for(i=0; i<SIZE; i++){		
+		printf("점수를 입력하세요. \n");
 		scanf("%d",&grade[i]);
+		sum+=grade[i];
 	}
-	for( i=0; i<SIZE; i++){		
-		printf("grade[%d] = %d\n", i, grade[i]);
-	}
+	average = sum/SIZE;
+	printf("성적 평균: %d\n",average);
 	return 0;
 	
 }
