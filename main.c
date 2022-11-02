bb5b957ddbc712bdac41f530cd88a1bf39546d30
soo0300 <soo0300@naver.com>
2022-11-02 12:40:26 +0900

    실습_5

--- main.c
+++ main.c
@@ -1,31 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define SIZE 4
+#include<stdlib.h>
+#include<stdio.h>
+
+#define ROWS 3
+#define COLS 3 
 
 
-void square(int a[], int size);
-void print(int a[], int size);
-
-void square(int a[], int size){
-	int i;
-	for(i=0; i<size; i++){
-		a[i]=a[i]*a[i];
-	}
-}
-
-void print(int a[], int size){
-	int i;
-	for(i=0; i<size; i++){
-		printf("%3d",a[i]);
-	}
-	printf("\n");
+void add(int A[][COLS], int B[][COLS], int C[][COLS]){
+	int i; int j;
+	for(i=0; i<ROWS; i++){
+		for(j=0; j<COLS; j++){
+			C[i][j]=A[i][j]+B[i][j];
+		}
 	
-}
+	}
+};
+
+
+void show(int C[][COLS]){
+	int i; int j;
+	for(i=0; i<ROWS; i++){
+		for(j=0; j<COLS; j++){
+			printf("%d ",C[i][j]);
+		}
+		printf("\n");
+	}
+};
+
+
 int main(void){
+	int i=0; int j=0;
+
+	int A[ROWS][COLS]={	{2,3,0}, {8,9,1}, {7,0,5} };
+	int B[ROWS][COLS]={ {1,0,0}, {0,1,0}, {0,0,1} };
+
+	int C[ROWS][COLS];
 	
-	int list[SIZE]={1,2,3,4};
-	square(list,SIZE);
-	print(list,SIZE);
+		
+	add(A,B,C);
+	show(C);
+	
+	
 	return 0;
 
+	
 }
